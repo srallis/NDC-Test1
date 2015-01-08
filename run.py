4961f35974a3daa0e402c2de4b6d59a5927e9bee
srallis1 <srallis@users.noreply.github.com>
2015-01-08 14:12:15 +0000

    microblog

--- run.py
+++ run.py
@@ -1,8 +1,4 @@
 import os
-
-from flask import Flask
-
-app = Flask(__name__)
-from app import view
+from app import app 
 
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)),debug=True)
